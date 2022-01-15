eef0a4b1942207e6da5e60c2252bc108ea705377
viniciustr19 <viniciustr19@gmail.com>
2022-01-15 18:14:49 -0300

    z .

---
+++ Primeiros passos com .NET/DigitalInnovationOne/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int numeroDeVezes = 10;
+
+            for(int i=0; i< numeroDeVezes; i++)
+            {
+                Console.WriteLine($"Bem-viindo ao curso de .NET {i}");
+            }
+        }
+    }
+}
